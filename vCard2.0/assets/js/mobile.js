fef7b1047f47da7f4ea8275bf14e77fbc423d634
Neller <Oliver.Neller@nord.com>
2019-08-02 13:50:48 +0200

    Contact page is created

--- vCard2.0/assets/js/mobile.js
+++ vCard2.0/assets/js/mobile.js
@@ -2,7 +2,14 @@ $(document).ready(function() {
     $('.toggleOpen').click(function() {
         $("header nav ul").toggleClass("open");
     });
-    $('.nav-item').click(function() {
-        $("header nav ul").toggleClass("open");
+    $('.toggleOpen').click(function() {
+        $(".fa-bars").toggleClass("open");
+    });
+    $('.toggleOpen').click(function() {
+        $(".fa-times").toggleClass("open");
+    });
+    $('.button').click(function(){
+        location.reload();
+        alert("E-Mail wurde Versendet!");
     });
 });
---
+++ vCard2.0/contact-form-handler.php
@@ -0,0 +1,28 @@
+<?php
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['text'];
+    $user_subject = $_POST['subject'];
+
+
+    $mail_form = 'o.neller@web.de';
+
+    $email_subject = 'NEUE EMAIL VON LOCALHOST';
+
+    $email_body = "NAME: $fname $lname. \n".
+                    "EMail ADRESSE: $visitor_email. \n".
+                        "BETREFF: $user_subject. \n".
+                            "NACHRICHT: $message. \n";
+
+    
+    $to = "oliver.neller@nord.com";
+
+    $headers = "Form $email_form \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+    
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+?>
