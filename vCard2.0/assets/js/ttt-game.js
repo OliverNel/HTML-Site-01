b471069d38699e510b658db73e66b598e3b17683
Neller <Oliver.Neller@nord.com>
2019-08-05 11:34:08 +0200

    add game page and new js file

---
+++ vCard2.0/assets/js/ttt-game.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    $('.td-or').click(function() {
+        $(".td-or .x").toggleClass("open-click");
+    });
+    $('.td-or').click(function() {
+        $(".td-or .o").toggleClass("open-click");
+    });
+
+
+    $('.td-om').click(function() {
+        $(".td-om .x").toggleClass("open-click");
+    });
+    $('.td-om').click(function() {
+        $(".td-om .o").toggleClass("open-click");
+    });
+
+
+    $('.td-ol').click(function() {
+        $(".td-ol .x").toggleClass("open-click");
+    });
+    $('.td-ol').click(function() {
+        $(".td-ol .o").toggleClass("open-click");
+    });
+
+
+    $('.td-mr').click(function() {
+        $(".td-mr .x").toggleClass("open-click");
+    });
+    $('.td-mr').click(function() {
+        $(".td-mr .o").toggleClass("open-click");
+    });
+
+
+    $('.td-mm').click(function() {
+        $(".td-mm .x").toggleClass("open-click");
+    });
+    $('.td-mm').click(function() {
+        $(".td-mm .o").toggleClass("open-click");
+    });
+
+
+    $('.td-ml').click(function() {
+        $(".td-ml .x").toggleClass("open-click");
+    });
+    $('.td-ml').click(function() {
+        $(".td-ml .o").toggleClass("open-click");
+    });
+
+
+    $('.td-ur').click(function() {
+        $(".td-ur .x").toggleClass("open-click");
+    });
+    $('.td-ur').click(function() {
+        $(".td-ur .o").toggleClass("open-click");
+    });
+
+
+    $('.td-um').click(function() {
+        $(".td-um .x").toggleClass("open-click");
+    });
+    $('.td-um').click(function() {
+        $(".td-um .o").toggleClass("open-click");
+    });
+
+
+    $('.td-ul').click(function() {
+        $(".td-ul .x").toggleClass("open-click");
+    });
+    $('.td-ul').click(function() {
+        $(".td-ul .o").toggleClass("open-click");
+    });
+});
